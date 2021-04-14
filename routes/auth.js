bbcebca8701e22393dfc28ee09a7ba40102dc3d7
Talal <talalabbas84@gmail.com>
2021-04-14 12:10:31 +0500

    FDDF

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,10 @@ const {
   login,
   verifyEmail,
   getUsers,
-<<<<<<< HEAD
   forgetPassword,
   resendResetCode,
   verifyResetCode,
   updatePasswordAfterCode,
-=======
->>>>>>> 4366774b1393ef5f6fbba5cf68afe93abe95831e
   resendVerificationCode,
 } = require("../controllers/auth");
 
@@ -21,14 +18,10 @@ router.post("/register", register);
 router.post("/login", login);
 router.post("/email-verification", protect, verifyEmail);
 router.get("/get-users", getUsers);
-<<<<<<< HEAD
 router.post("/send-forget-password-verification-code", forgetPassword);
 router.put("/resend-forget-password-verification-code", resendResetCode);
 router.post("/verify-forget-password-verification-code", verifyResetCode);
 router.put("/update-password-after-verification-code", updatePasswordAfterCode);
 router.put("/resend-email-verification-code", protect, resendVerificationCode);
 
-=======
-router.get("/resendverificationcode", resendVerificationCode);
->>>>>>> 4366774b1393ef5f6fbba5cf68afe93abe95831e
 module.exports = router;
