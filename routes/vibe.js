467b16cb9b1cc726b614db0ad8c2ddb404fe9a79
Talal <talalabbas84@gmail.com>
2021-06-10 00:23:46 +0500

    DasdsaD

--- routes/vibe.js
+++ routes/vibe.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
-const { createVibe } = require("../controllers/vibe");
+const { createVibe, getVibes } = require("../controllers/vibe");
 
 const { protect } = require("../middleware/auth");
 const { route } = require("./auth");
+
+router.get("/", getVibes);
 router.post("/create-vibe", protect, createVibe);
 
 module.exports = router;
