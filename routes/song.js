b158aafe5d73dcb488b98e675a3424fdaf55951e
Talal <talalabbas84@gmail.com>
2021-06-25 01:28:40 +0500

    FSDFDS

--- routes/song.js
+++ routes/song.js
@@ -1,17 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 const {
   addSong,
   getSongs,
   getSongById,
-  getSongbyLyrics
-} = require('../controllers/song');
+  getSongbyLyrics,
+} = require("../controllers/song");
 
-const { protect } = require('../middleware/auth');
-const { route } = require('./auth');
-router.post('/get-song-by-lyrics', getSongbyLyrics);
-router.post('/add-song', addSong);
-router.get('/', getSongs);
-router.get('/:id', getSongById);
+const { protect } = require("../middleware/auth");
+const { route } = require("./auth");
+router.post("/get-song-by-lyrics", protect, getSongbyLyrics);
+router.post("/add-song", protect, addSong);
+router.get("/", getSongs);
+router.get("/:id", getSongById);
 
 module.exports = router;
